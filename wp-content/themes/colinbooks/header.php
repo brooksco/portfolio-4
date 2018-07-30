8045fd759bedbc470a7d252fb460e2cd7fdd337f
Colin Brooks <colinbooks@gmail.com>
2018-07-30 01:05:55 -0400

    wip

--- wp-content/themes/colinbooks/header.php
+++ wp-content/themes/colinbooks/header.php
@@ -33,6 +33,7 @@ dmmmmmmd+.      hmmmmm:     +dmdo-` .hmmmmmd.
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="#000000">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
 	<?php wp_head(); ?>
