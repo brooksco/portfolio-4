569f23b825726410b1b5c0ec31e16fce7516cfd8
Colin Brooks <colinbooks@gmail.com>
2018-07-30 23:53:10 -0400

    updated

--- wp-content/themes/colinbooks/header.php
+++ wp-content/themes/colinbooks/header.php
@@ -36,7 +36,17 @@ dmmmmmmd+.      hmmmmm:     +dmdo-` .hmmmmmd.
     <meta name="theme-color" content="#000000">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
+    <?php wp_head(); ?>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-41574595-2"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-41574595-2');
+    </script>
 </head>
 
 <body <?php body_class(); ?>>
